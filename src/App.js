1fa3f8280e2bb25c0d92d61266d21b3020e4196e
Eljay <zogzmiller@gmail.com>
2023-01-16 07:42:48 -0600

    testingera

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning React
+          Learning to React
         </a>
       </header>
     </div>
